f2169a4b7ecaaf6e2cfd5ecf220f904a1dc2dfff
iamsubroto <smartcoder500@gmail.com>
2019-04-11 02:40:01 -0700

    chnages bro

---
+++ build/precache-manifest.b4b17acf03faee22c9a695bab04ef09b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/templatebazar/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1a07f49cb620172c1034",
+    "url": "/templatebazar/static/js/runtime~main.4bf7a3f5.js"
+  },
+  {
+    "revision": "2eab52da55f051383b1a",
+    "url": "/templatebazar/static/js/main.31ab69f7.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/templatebazar/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "2eab52da55f051383b1a",
+    "url": "/templatebazar/static/css/main.07318036.chunk.css"
+  },
+  {
+    "revision": "46fe29fcabd1564b5c3865c2334013cb",
+    "url": "/templatebazar/index.html"
+  }
+];
