483959b153972a8824ebec4dd10b099f76a79407
iamsubroto <smartcoder500@gmail.com>
2019-04-11 03:02:00 -0700

    navbar color chnages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 class Navbar extends Component {
   render() {
     return (
-        <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
             <div className="container">
                         <a class="navbar-brand" href="#">Navbar</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
